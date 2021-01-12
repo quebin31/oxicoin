399353375a765b3a88b3589d9767ede3b826a270
Kevin Del Castillo <quebin31@gmail.com>
2021-01-12 14:03:30 -0500

    Added add, sub, mul, pow{u,i} and div for FieldElem.

--- src/field.rs
+++ src/field.rs
@@ -1,17 +1,105 @@
-use crate::Error;
+use std::ops::{Add, Div, Mul, Sub};
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+use crate::{Error, Result};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct FieldElem {
     number: usize,
     prime: usize,
 }
 
 impl FieldElem {
-    pub fn new(number: usize, prime: usize) -> Result<Self, Error> {
+    /// Build a new field element `number` on F_{prime}.
+    pub fn new(number: usize, prime: usize) -> Result<Self> {
         if number >= prime {
             Err(Error::InvalidFieldNumber(number, prime))
         } else {
             Ok(Self { number, prime })
+        }
+    }
+
+    /// Get the _additive inverse_ of this element.
+    #[inline]
+    pub fn add_inv(self) -> Self {
+        let number = self.prime - self.number;
+        Self { number, ..self }
+    }
+
+    /// Get the _multiplicative inverse_ of this element.
+    #[inline]
+    pub fn mul_inv(self) -> Self {
+        // Fermat's little theorem
+        self.powu(self.prime - 2)
+    }
+
+    pub fn powu(self, exp: usize) -> Self {
+        let mut number = 1;
+        for _ in 0..exp {
+            number *= self.number;
+            number %= self.prime;
+        }
+
+        Self { number, ..self }
+    }
+
+    pub fn powi(self, exp: isize) -> Self {
+        // Rust's `%` operator is not the modulus operation.
+        let exp = exp.rem_euclid(self.prime as isize - 1);
+        self.powu(exp as usize)
+    }
+}
+
+impl Add for FieldElem {
+    type Output = Result<Self>;
+
+    #[inline]
+    fn add(self, rhs: Self) -> Self::Output {
+        if self.prime != rhs.prime {
+            Err(Error::InvalidFieldAddition)
+        } else {
+            let number = (self.number + rhs.number) % self.prime;
+            Ok(Self { number, ..self })
+        }
+    }
+}
+
+impl Sub for FieldElem {
+    type Output = Result<Self>;
+
+    #[inline]
+    fn sub(self, rhs: Self) -> Self::Output {
+        if self.prime != rhs.prime {
+            Err(Error::InvalidFieldSubstraction)
+        } else {
+            self.add(rhs.add_inv())
+        }
+    }
+}
+
+impl Mul for FieldElem {
+    type Output = Result<Self>;
+
+    #[inline]
+    fn mul(self, rhs: Self) -> Self::Output {
+        if self.prime != rhs.prime {
+            Err(Error::InvalidFieldMultiplication)
+        } else {
+            // quebin31: what happens if we overflow?
+            let number = (self.number * rhs.number) % self.prime;
+            Ok(Self { number, ..self })
+        }
+    }
+}
+
+impl Div for FieldElem {
+    type Output = Result<Self>;
+
+    #[inline]
+    fn div(self, rhs: Self) -> Self::Output {
+        if self.prime != rhs.prime {
+            Err(Error::InvalidFieldDivition)
+        } else {
+            self.mul(rhs.mul_inv())
         }
     }
 }
@@ -29,6 +117,57 @@ mod tests {
         assert_ne!(a, b);
         assert_eq!(b, b);
 
+        Ok(())
+    }
+
+    #[test]
+    fn addition() -> Result<()> {
+        let a = FieldElem::new(6, 13)?;
+        let b = FieldElem::new(7, 13)?;
+        let c = a.add(b)?;
+
+        assert_eq!(c, FieldElem::new(0, 13)?);
+        Ok(())
+    }
+
+    #[test]
+    fn substraction() -> Result<()> {
+        let a = FieldElem::new(9, 57)?;
+        let b = FieldElem::new(29, 57)?;
+        let c = a.sub(b)?;
+
+        assert_eq!(c, FieldElem::new(37, 57)?);
+        Ok(())
+    }
+
+    #[test]
+    fn multiplication() -> Result<()> {
+        let a = FieldElem::new(3, 13)?;
+        let b = FieldElem::new(12, 13)?;
+        let c = a.mul(b)?;
+
+        assert_eq!(c, FieldElem::new(10, 13)?);
+        Ok(())
+    }
+
+    #[test]
+    fn exponentiation() -> Result<()> {
+        let a = FieldElem::new(7, 13)?;
+        let b = a.powu(3);
+        let c = a.powi(-3);
+
+        assert_eq!(b, FieldElem::new(5, 13)?);
+        assert_eq!(c, FieldElem::new(8, 13)?);
+        Ok(())
+    }
+
+    #[test]
+    fn divition() -> Result<()> {
+        let a = FieldElem::new(2, 19)?;
+        let b = FieldElem::new(7, 19)?;
+        let c = a.div(b)?;
+
+        assert_eq!(c, FieldElem::new(3, 19)?);
         Ok(())
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,18 @@ use thiserror::Error;
 pub enum Error {
     #[error("Invalid field number {0}, not in range 0..{1}")]
     InvalidFieldNumber(usize, usize),
+
+    #[error("Invalid addition on different fields")]
+    InvalidFieldAddition,
+
+    #[error("Invalid substraction on different fields")]
+    InvalidFieldSubstraction,
+
+    #[error("Invalid multiplication on different fields")]
+    InvalidFieldMultiplication,
+
+    #[error("Invalid divition on different fields")]
+    InvalidFieldDivition,
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
