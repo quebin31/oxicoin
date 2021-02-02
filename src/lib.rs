3cd8ac264563e07d0cf07c008ef328d3e0952056
Kevin Del Castillo <quebin31@gmail.com>
2021-02-01 23:29:12 -0500

    Provide custom error builder and impl dummy from Infallible.

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,51 @@
 pub mod curve;
 pub mod field;
 pub mod traits;
-pub mod utils;
 
-use std::error::Error as StdError;
+use std::convert::Infallible;
+
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("Something went wrong: {source}")]
-    AnyError { source: anyhow::Error },
+    #[error("custom error")]
+    Custom(String),
 
-    #[error("Invalid field number {0}, not in range 0..{1}")]
-    InvalidFieldNumber(usize, usize),
+    #[error("invalid field number, not in range")]
+    InvalidFieldNumber,
 
-    #[error("Invalid addition on different fields")]
+    #[error("invalid addition on different fields")]
     InvalidFieldAddition,
 
-    #[error("Invalid substraction on different fields")]
+    #[error("invalid substraction on different fields")]
     InvalidFieldSubstraction,
 
-    #[error("Invalid multiplication on different fields")]
+    #[error("invalid multiplication on different fields")]
     InvalidFieldMultiplication,
 
-    #[error("Invalid divition on different fields")]
+    #[error("invalid divition on different fields")]
     InvalidFieldDivition,
 
-    #[error("Point is not on the curve")]
+    #[error("point is not on the curve")]
     PointNotInTheCurve,
 
-    #[error("Received an invalid ec point")]
+    #[error("received an invalid ec point")]
     InvalidECPoint,
 
-    #[error("Points are not in the same curve")]
+    #[error("points are not in the same curve")]
     PointsNotInTheSameCurve,
 }
 
 impl Error {
-    pub fn from_err<E>(err: E) -> Self
-    where
-        E: StdError + Send + Sync + 'static,
-    {
-        Self::AnyError { source: err.into() }
+    pub fn custom<T: ToString>(msg: T) -> Self {
+        Self::Custom(msg.to_string())
     }
 }
 
-#[derive(Debug, Clone, Error)]
-#[error("Infallible error!")]
-pub struct Infallible;
+impl From<Infallible> for Error {
+    fn from(_: Infallible) -> Self {
+        unreachable!()
+    }
+}
 
 pub type Result<T> = std::result::Result<T, Error>;
