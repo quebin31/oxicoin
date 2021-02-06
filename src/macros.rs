d5d0a777686c42cb382c007d26eadacac8fc1f55
Kevin Del Castillo <quebin31@gmail.com>
2021-02-06 15:18:11 -0500

    Add macro to forward binary operation implementation.

---
+++ src/macros.rs
@@ -0,0 +1,28 @@
+#[macro_export]
+macro_rules! forward_binop_impl {
+    (for non-copyable $type:ty where $binop:ident does $met:ident) => {
+        impl<'a> $binop<&'a $type> for $type {
+            type Output = $type;
+
+            fn $met(self, rhs: &'a $type) -> Self::Output {
+                $binop::$met(&self, rhs)
+            }
+        }
+
+        impl<'a> $binop<$type> for &'a $type {
+            type Output = $type;
+
+            fn $met(self, rhs: $type) -> Self::Output {
+                $binop::$met(self, &rhs)
+            }
+        }
+
+        impl $binop for $type {
+            type Output = $type;
+
+            fn $met(self, rhs: $type) -> Self::Output {
+                $binop::$met(&self, &rhs)
+            }
+        }
+    };
+}
