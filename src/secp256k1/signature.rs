91032326fd42bc7d18f8bc02ed7a3d6be1335c28
Kevin Del Castillo <quebin31@gmail.com>
2021-02-13 22:38:30 -0500

    Add serialize method for `Signature`.
    Add unit tests for serialize.

--- src/secp256k1/signature.rs
+++ src/secp256k1/signature.rs
@@ -1,5 +1,6 @@
 use num_bigint::BigUint;
 
+use crate::utils::strip_start;
 use crate::Error;
 
 use super::crypto::PublicKey;
@@ -38,5 +39,66 @@ impl Signature {
 
         let total = &*G * u + &pub_key.ec_point * v;
         Ok(total.x().unwrap().0 == self.r)
+    }
+
+    /// Serialize signature with DER format
+    pub fn serialize(&self) -> Result<Vec<u8>, Error> {
+        let r_bigendian = self.r.to_bytes_be();
+        let r_bigendian = strip_start(&r_bigendian, 0x00);
+        let r_bigendian = if r_bigendian[0] & 0x80 == 0x80 {
+            std::iter::once(0x00u8)
+                .chain(r_bigendian.iter().copied())
+                .collect::<Vec<_>>()
+        } else {
+            r_bigendian.to_vec()
+        };
+
+        let prefix = [0x02u8, r_bigendian.len() as u8];
+        let result = prefix.iter().copied().chain(r_bigendian);
+
+        let s_bigendian = self.s.to_bytes_be();
+        let s_bigendian = strip_start(&s_bigendian, 0x00);
+        let s_bigendian = if s_bigendian[0] & 0x80 == 0x80 {
+            std::iter::once(0x00u8)
+                .chain(s_bigendian.iter().copied())
+                .collect::<Vec<_>>()
+        } else {
+            s_bigendian.to_vec()
+        };
+
+        let result = result
+            .chain([0x02u8, s_bigendian.len() as u8].iter().copied())
+            .chain(s_bigendian)
+            .collect::<Vec<_>>();
+
+        let serialized = [0x30u8, result.len() as u8]
+            .iter()
+            .copied()
+            .chain(result)
+            .collect();
+
+        Ok(serialized)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use hex_literal::hex;
+
+    use super::Signature;
+
+    #[test]
+    fn der_format() {
+        let r = biguint!("37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6");
+        let s = biguint!("8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec");
+        let signature = Signature::new(r, s);
+
+        let serialized = signature.serialize().unwrap();
+        let expected = hex!(
+            "3045022037206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6022100
+            8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec"
+        );
+
+        assert_eq!(serialized, expected);
     }
 }
