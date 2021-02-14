373ee52f871fef2504df30ad81f193751afc23fa
Kevin Del Castillo <quebin31@gmail.com>
2021-02-13 19:28:20 -0500

    Change signatures and remove unit tests.
    Change signatures to be more generic.
    Remove unit tests in favor of integration tests.

--- src/secp256k1/signature.rs
+++ src/secp256k1/signature.rs
@@ -1,5 +1,7 @@
 use num_bigint::BigUint;
 
+use crate::Error;
+
 use super::crypto::PublicKey;
 use super::{G, N};
 
@@ -19,7 +21,15 @@ impl Signature {
         Self { r, s }
     }
 
-    pub fn is_valid(&self, digest: &[u8; 32], pub_key: &PublicKey) -> bool {
+    pub fn is_valid<B>(&self, digest: B, pub_key: &PublicKey) -> Result<bool, Error>
+    where
+        B: AsRef<[u8]>,
+    {
+        let digest = digest.as_ref();
+        if digest.len() != 32 {
+            return Err(Error::InvalidDigestLength(digest.len()));
+        }
+
         let z = BigUint::from_bytes_be(digest);
         let s_inv = self.s.modpow(&(&*N - 2usize), &*N);
 
@@ -27,30 +37,6 @@ impl Signature {
         let v = (&self.r * &s_inv) % &*N;
 
         let total = &*G * u + &pub_key.ec_point * v;
-        total.x().unwrap().0 == self.r
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::secp256k1::crypto::PublicKey;
-
-    use super::Signature;
-
-    use hex_literal::hex;
-
-    #[test]
-    fn must_be_valid() {
-        let digest = hex!("bc62d4b80d9e36da29c16c5d4d9f11731f36052c72401a76c23c0fb5a9b74423");
-
-        let r = biguint!("37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6");
-        let s = biguint!("8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec");
-        let signature = Signature::new(r, s);
-
-        let x = biguint!("04519fac3d910ca7e7138f7013706f619fa8f033e6ec6e09370ea38cee6a7574");
-        let y = biguint!("82b51eab8c27c66e26c858a079bcdf4f1ada34cec420cafc7eac1a42216fb6c4");
-        let pub_key = PublicKey::new(x, y).unwrap();
-
-        assert!(signature.is_valid(&digest, &pub_key))
+        Ok(total.x().unwrap().0 == self.r)
     }
 }
